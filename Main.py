d5e10fb0cc749ba82c4eb7e6b3ca9d84e20a4c75
Khairafarida <khairafarida0@gmail.com>
2026-01-12 13:48:05 +0700

    update main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
-
 secret_word = "cat"
 
 letter = input("Masukkan satu huruf: ")
 
 if len(letter) == 1 and letter.isalpha():
-
     if letter.lower() in secret_word.lower():
         print("Huruf tersebut ADA di dalam kata rahasia.")
     else:
