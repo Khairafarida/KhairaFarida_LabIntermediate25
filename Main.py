69194714aa6170653ca077a60abae2f92e04d246
Khairafarida <khairafarida0@gmail.com>
2026-01-12 14:22:20 +0700

    update main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,19 @@
 secret_word = "cat"
+guessed_letters = set()
 
-letter = input("Masukkan satu huruf: ")
+while True:
+    letter = input("Masukkan satu huruf: ")
 
-if len(letter) == 1 and letter.isalpha():
-    if letter.lower() in secret_word.lower():
-        print("Huruf tersebut ADA di dalam kata rahasia.")
+    if len(letter) == 1 and letter.isalpha():
+        if letter.lower() in secret_word.lower():
+            guessed_letters.add(letter.lower())
+            print("Huruf tersebut ADA di dalam kata rahasia.")
+        else:
+            print("Huruf tersebut TIDAK ADA di dalam kata rahasia.")
     else:
-        print("Huruf tersebut TIDAK ADA di dalam kata rahasia.")
-else:
-    print("Input tidak valid! Masukkan tepat satu huruf saja.")
+        print("Input tidak valid! Masukkan tepat satu huruf saja.")
+
+    if set(secret_word) == guessed_letters:
+        print("\nSemua huruf berhasil ditebak!")
+        print("Kata rahasia adalah:", secret_word)
+        break
